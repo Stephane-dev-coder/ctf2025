ca1d1664324d987fbe6716c5e6cadb3d0517d933
stebzzz <145580377+stebzzz@users.noreply.github.com>
2025-02-04 16:52:18 +0100

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
     extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
+=======
+>>>>>>> parent of ef9ca51 (netlify deploy)
   optimizeDeps: {
     include: ['firebase/app', 'firebase/auth', 'firebase/firestore']
   },
   build: {
+<<<<<<< HEAD
     outDir: 'dist',
     sourcemap: true,
     rollupOptions: {
@@ -22,5 +25,8 @@ export default defineConfig({
         main: path.resolve(__dirname, 'index.html')
       }
     }
+=======
+    sourcemap: true
+>>>>>>> parent of ef9ca51 (netlify deploy)
   }
 });
